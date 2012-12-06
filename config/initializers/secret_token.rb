c3f9213dc15cabba36b66943e6e1ad82c1cc3a1b
Stephanie Cheng <stephanie@jugnoo.com>
2012-12-06 05:39:08 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatissystems::Application.config.secret_token = '27b54dcc7f1c533269a8819d2cd7444e23dabf06de6051fe34e290c9c05d75cb5df8fd95564e13433026d9cfbfaa6b61471a58a8e4daf9f0d779aaba7e9d2bf3'
