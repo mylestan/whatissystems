4d32a3342b0096907e70414757b7a6e2d804f295
Myles Tan <mylestan7@gmail.com>
2013-07-16 01:14:57 -0400

    added js object for storing content

---
+++ res/page-content.js
@@ -0,0 +1,48 @@
+var content = {
+	aboutPage : {
+		title : 'About',
+		tiles : [
+			{
+				title : 'From the University',
+				content : '<p>Our university has a great description of our program. We highly recommend you check it out</p><p>You can click <a href="http://uwaterloo.ca" target="_blank">here.</a></p>'
+			},
+			{
+				title : 'Initiatives',
+				content: '<p>I have no idea what Josh has for this!</p>'
+			},
+			{
+				title : 'SYDE vs. Other',
+				content: '<p>We are different!</p>'
+			},
+			{
+				title : 'Projects',
+				content: '<p>We focus on design, and make cool projects.</p>'
+			}
+		]
+	},
+	experiencePage : {
+		title: 'Expeirience',
+		tiles : [
+			{
+				title : 'E5',
+				content : '<p>We have a nice floor in E5</p>'
+			},
+			{
+				title : 'Coffee House',
+				content : '<p>We\'re so social, we have our OWN Coffee House.</p>'
+			},
+			{
+				title : 'All Systems Go',
+				content : '<p>We can\'t talk much about this. But it\'s fun.<p>'
+			},
+			{
+				title : 'Profs Across the Hall',
+				content : '<p>I have never taken advantage of this. But apparently it is nice.</p>'
+			},
+			{
+				title : 'Iron Ring Shenanagains',
+				content : '<p>We do cool things? Don\'t all faculties do cool things?</p>'
+			}
+		]
+	}
+}
