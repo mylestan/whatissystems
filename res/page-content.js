3fd27574bb0cac12cf852413cab64de65a3cfb66
Josh Cheung <joshua.cheung21@gmail.com>
2013-07-16 23:50:23 -0400

    added basic content and uploaded images

--- res/page-content.js
+++ res/page-content.js
@@ -3,20 +3,20 @@ var content = {
 		title : 'About',
 		tiles : [
 			{
-				title : 'From the University',
-				content : '<p>Our university has a great description of our program. We highly recommend you check it out</p><p>You can click <a href="http://uwaterloo.ca" target="_blank">here.</a></p>'
+				title : 'What the School Says',
+				content : '<p>Our university has a great description of our program, which you can check out</p><p>You can click <a href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering" target="_blank">here.</a></p>'
 			},
 			{
 				title : 'Initiatives',
-				content: '<p>I have no idea what Josh has for this!</p>'
+				content: '<p>The program\'s innovative culture breeds entrepreneurs looking to make a dent in the world. Check out these cool companies started by systems graduates! (Infographic)</p>'
 			},
 			{
 				title : 'SYDE vs. Other',
-				content: '<p>We are different!</p>'
+				content: '<p>What makes us different from the traditional engineering disciplines? The numbers show it all. (Stats)</p>'
 			},
 			{
 				title : 'Projects',
-				content: '<p>We focus on design, and make cool projects.</p>'
+				content: '<p>Every year we apply design and technical knowledge by making cool projects. Check out 4th year design projects <a href="https://uwaterloo.ca/systems-design-engineering/workshop-projects" target="_blank">here.</a></p>'
 			}
 		]
 	},
@@ -24,24 +24,20 @@ var content = {
 		title: 'Expeirience',
 		tiles : [
 			{
-				title : 'E5',
-				content : '<p>We have a nice floor in E5</p>'
+				title : 'The 6th Floor of E5',
+				content : '<p>All classes in every year are in the top floor in Engineering 5, one of the newest buildings on campus. We\'re spoiled with vending machines, a lounge, and a sweet view. You\'ll be sure to bump into upper years in the hallway! (Images)</p>'
 			},
 			{
-				title : 'Coffee House',
-				content : '<p>We\'re so social, we have our OWN Coffee House.</p>'
+				title : 'Coffeehouse',
+				content : '<p>The diversity and hidden talents of systems students are brought out during the Systems Coffeehouse, a talent show held apart from the one the Faculty of Engineering runs. It happens every term as a great break after midterms, and on occasion, teaching assistants and professors will showcase their talent! (Images)</p>'
 			},
 			{
 				title : 'All Systems Go',
-				content : '<p>We can\'t talk much about this. But it\'s fun.<p>'
-			},
-			{
-				title : 'Profs Across the Hall',
-				content : '<p>I have never taken advantage of this. But apparently it is nice.</p>'
+				content : '<p>It\'s been said that systems students are among the most social engineers. We prove our point at the beginning of each term by running All Systems Go, a gathering of systems students at a residence of one of the students. It\'s a great opportunity to meet upper year students! Unfortunately to keep this site professional, we can\'t show pictures.<p>'
 			},
 			{
 				title : 'Iron Ring Shenanagains',
-				content : '<p>We do cool things? Don\'t all faculties do cool things?</p>'
+				content : '<p>To celebrate receiving the coveted iron ring and 5 years of blood, sweat, and tears, systems graduates pull off pranks on the 6th floor of E5. (Images)</p>'
 			}
 		]
 	}
