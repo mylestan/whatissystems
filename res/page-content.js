dad4373cac4ae649c77cf0523fe150619286092a
Myles Tan <mylestan7@gmail.com>
2013-07-20 14:34:04 -0400

    popup works, some functionality on adding event listeners to the tiles dynamically

--- res/page-content.js
+++ res/page-content.js
@@ -1,44 +1,50 @@
 var content = {
-	aboutPage : {
-		title : 'About',
-		tiles : [
-			{
-				title : 'What the School Says',
-				content : '<p>Our university has a great description of our program, which you can check out</p><p>You can click <a href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering" target="_blank">here.</a></p>'
-			},
-			{
-				title : 'Initiatives',
-				content: '<p>The program\'s innovative culture breeds entrepreneurs looking to make a dent in the world. Check out these cool companies started by systems graduates! (Infographic)</p>'
-			},
-			{
-				title : 'SYDE vs. Other',
-				content: '<p>What makes us different from the traditional engineering disciplines? The numbers show it all. (Stats)</p>'
-			},
-			{
-				title : 'Projects',
-				content: '<p>Every year we apply design and technical knowledge by making cool projects. Check out 4th year design projects <a href="https://uwaterloo.ca/systems-design-engineering/workshop-projects" target="_blank">here.</a></p>'
-			}
-		]
+	'what-the-school-says' : {
+		'section' : 'about',
+		'cssId' : 'what-the-school-says',
+		'detailTitle' : 'What the School Says',
+		'detailContent' : '<p>Our university has a great description of our program, which you can check out</p><p>You can click <a href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering" target="_blank">here.</a></p>'
 	},
-	experiencePage : {
-		title: 'Expeirience',
-		tiles : [
-			{
-				title : 'The 6th Floor of E5',
-				content : '<p>All classes in every year are in the top floor in Engineering 5, one of the newest buildings on campus. We\'re spoiled with vending machines, a lounge, and a sweet view. You\'ll be sure to bump into upper years in the hallway! (Images)</p>'
-			},
-			{
-				title : 'Coffeehouse',
-				content : '<p>The diversity and hidden talents of systems students are brought out during the Systems Coffeehouse, a talent show held apart from the one the Faculty of Engineering runs. It happens every term as a great break after midterms, and on occasion, teaching assistants and professors will showcase their talent! (Images)</p>'
-			},
-			{
-				title : 'All Systems Go',
-				content : '<p>It\'s been said that systems students are among the most social engineers. We prove our point at the beginning of each term by running All Systems Go, a gathering of systems students at a residence of one of the students. It\'s a great opportunity to meet upper year students! Unfortunately to keep this site professional, we can\'t show pictures.<p>'
-			},
-			{
-				title : 'Iron Ring Shenanagains',
-				content : '<p>To celebrate receiving the coveted iron ring and 5 years of blood, sweat, and tears, systems graduates pull off pranks on the 6th floor of E5. (Images)</p>'
-			}
-		]
+	'initiatives' : {
+		'section' : 'about',
+		'cssId' : "initiatives",
+		'detailTitle' : 'Initiatives',
+		'detailContent': '<p>The program\'s innovative culture breeds entrepreneurs looking to make a dent in the world. Check out these cool companies started by systems graduates! (Infographic)</p>'
+	},
+	'syde-vs-the-rest' : {
+		'section' : 'about',
+		'cssId' : 'syde-vs-the-rest',
+		'detailTitle' : 'SYDE v.s. The Rest',
+		'detailContent': '<p>What makes us different from the traditional engineering disciplines? The numbers show it all. (Stats)</p>'
+	},
+	'design-projects' : {
+		'section' : 'about',
+		'cssId' : 'design-projects',
+		'detailTitle' : 'Design Projects',
+		'detailContent': '<p>Every year we apply design and technical knowledge by making cool projects. Check out 4th year design projects <a href="https://uwaterloo.ca/systems-design-engineering/workshop-projects" target="_blank">here.</a></p>'
+	},
+	'the-6th-floor-of-e5' : {
+		'section' : 'experience',
+		'cssId' : 'the-6th-floor-of-e5',
+		'detailTitle' : 'The 6th Floor of E5',
+		'detailContent' : '<p>All classes in every year are in the top floor in Engineering 5, one of the newest buildings on campus. We\'re spoiled with vending machines, a lounge, and a sweet view. You\'ll be sure to bump into upper years in the hallway! (Images)</p>'
+	},
+	'coffeehouse' : {
+		'section' : 'experience',
+		'cssId' : 'coffeehouse',
+		'detailTitle' : 'Coffeehouse',
+		'detailContent' : '<p>The diversity and hidden talents of systems students are brought out during the Systems Coffeehouse, a talent show held apart from the one the Faculty of Engineering runs. It happens every term as a great break after midterms, and on occasion, teaching assistants and professors will showcase their talent! (Images)</p>'
+	},
+	'all-systems-go' : {
+		'section' : 'experience',
+		'cssId' : 'all-systems-go',
+		'detailTitle' : 'All Systems Go',
+		'detailContent' : '<p>It\'s been said that systems students are among the most social engineers. We prove our point at the beginning of each term by running All Systems Go, a gathering of systems students at a residence of one of the students. It\'s a great opportunity to meet upper year students! Unfortunately to keep this site professional, we can\'t show pictures.<p>'
+	},
+	'iron-ring-festivities' : {
+		'section' : 'experience',
+		'cssId' : 'iron-ring-festivities',
+		'detailTitle' : 'Iron Ring Festivities',
+		'detailContent' : '<p>To celebrate receiving the coveted iron ring and 5 years of blood, sweat, and tears, systems graduates pull off pranks on the 6th floor of E5. (Images)</p>'
 	}
 }
